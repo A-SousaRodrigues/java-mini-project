da78d2932fbebf6f778be24156667f4d3aed583a
Thomas Fearn <tomfearn03@gmail.com>
2024-11-22 12:34:06 +0000

    add psuedo code to scriplt.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Target Ids
+
+// event listener on submit button
+
+    //create row in table
+
+    //add delete button to the end of each row
+
+        //add event listener for each delete button
+
+        //add functionality to delete row
+
+    //add content from form to table
+
+    //add totals row with total under the expenses collumn
